7b94fc6910770611113c8ca77143298f3b634c4d
Yli <yliane.villot@hotmail.fr>
2025-11-30 17:36:24 +0100

    Amélioration SEO

--- MHRemoquage-vitrine/astro.config.mjs
+++ MHRemoquage-vitrine/astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from "@tailwindcss/vite";
 
 
 // https://astro.build/config
 export default defineConfig({
-      vite: {
+  site: 'https://www.full-depannage.fr',
+  integrations: [sitemap()],
+  vite: {
     plugins: [tailwindcss()],
   },
 });
